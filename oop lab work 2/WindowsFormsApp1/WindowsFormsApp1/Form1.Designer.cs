64147bf799e3c85fe40acfb98bb2ff77210a7cc9
Дарина <131813220+dasmik0@users.noreply.github.com>
2025-09-10 13:03:54 +0300

    add project
    додано проект

---
+++ oop lab work 2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMass = new System.Windows.Forms.TextBox();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtSatellites = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(32, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Назва планети:";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtName.Location = new System.Drawing.Point(206, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtMass
+            // 
+            this.txtMass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtMass.Location = new System.Drawing.Point(206, 71);
+            this.txtMass.Name = "txtMass";
+            this.txtMass.Size = new System.Drawing.Size(100, 20);
+            this.txtMass.TabIndex = 2;
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtDiameter.Location = new System.Drawing.Point(206, 105);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 3;
+            // 
+            // txtSatellites
+            // 
+            this.txtSatellites.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtSatellites.Location = new System.Drawing.Point(206, 137);
+            this.txtSatellites.Name = "txtSatellites";
+            this.txtSatellites.Size = new System.Drawing.Size(100, 20);
+            this.txtSatellites.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(32, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Маса (кг):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Location = new System.Drawing.Point(32, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Діаметр (км):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label4.Location = new System.Drawing.Point(32, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Кількість супутників:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._1642169449_2_abrakadabra_fun_p_kosmos_multyashnii_fon_2;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(308, 168);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 66);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Додати";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(32, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Список планет";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.Black;
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txtOutput.Location = new System.Drawing.Point(25, 266);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(358, 193);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._4657b98ace28ed4;
+            this.ClientSize = new System.Drawing.Size(643, 530);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSatellites);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.txtMass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMass;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtSatellites;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ oop lab work 2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<Planet> planets = new List<Planet>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обробник кнопки "Додати"
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Зчитуємо дані з форми
+                string name = txtName.Text;
+                double mass = double.Parse(txtMass.Text, CultureInfo.InvariantCulture);
+                double diameter = double.Parse(txtDiameter.Text, CultureInfo.InvariantCulture);
+                int satellites = int.Parse(txtSatellites.Text);
+
+                // Створюємо планету
+                Planet planet = new Planet(name, mass, diameter, satellites);
+
+                // Додаємо у список
+                planets.Add(planet);
+
+                // Виводимо список у текстове поле
+                txtOutput.Clear();
+                foreach (var p in planets)
+                {
+                    txtOutput.AppendText(p.ToString() + Environment.NewLine);
+                }
+
+                // Очищаємо поля вводу
+                txtName.Clear();
+                txtMass.Clear();
+                txtDiameter.Clear();
+                txtSatellites.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Перевірте правильність введених даних!", "Помилка");
+            }
+        }
+    }
+
+    // Клас "Планета"
+    public class Planet
+    {
+        public string Name { get; set; }      // Назва планети
+
+        // Закриті поля
+        private double mass;                  // маса (кг)
+        private double diameter;              // діаметр (км)
+
+        public int Satellites { get; set; }   // Кількість супутників
+
+        // Конструктор
+        public Planet(string name, double mass, double diameter, int satellites)
+        {
+            Name = name;
+            this.mass = mass;
+            this.diameter = diameter;
+            Satellites = satellites;
+        }
+
+        // Методи доступу до маси
+        public void SetMass(double value) => mass = value;
+        public double GetMass() => mass;
+
+        // Методи доступу до діаметра
+        public void SetDiameter(double value) => diameter = value;
+        public double GetDiameter() => diameter;
+
+        // Метод для текстового виводу
+        public override string ToString()
+        {
+            return $"{Name} | Маса: {mass:E2} кг | Діаметр: {diameter} км | Супутники: {Satellites}";
+        }
+    }
+}
